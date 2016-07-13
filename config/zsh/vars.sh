9482fbada58453c3588ba54be6a7c05018f6d37c
dennyabraham <dennyabraham@users.noreply.github.com>
2016-07-13 08:06:37 -0500

    add fixme

--- config/zsh/vars.sh
+++ config/zsh/vars.sh
@@ -1,4 +1,5 @@
 export EDITOR=$BREW_PREFIX/bin/nvim
 export VISUAL=$EDITOR
 export ZSH_CUSTOM=$HOME/.config/zsh
+# FIXME: remove this
 export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
