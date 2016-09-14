7d910c2344eb5463ea0eb7a399ca56b098acf021
dennyabraham <dennyabraham@users.noreply.github.com>
2016-09-13 22:13:03 -0500

    xset + keyboard settings on linux

--- tag-linux/config/zsh/arch.sh
+++ tag-linux/config/zsh/arch.sh
@@ -4,6 +4,12 @@ export LOGIN_COLOR=$(printf "%03d" $(((RANDOM % 230) + 1)))
 export TMUX_DEFAULT_COMMAND="$SHELL"
 
 # this might not work outside of a running x instance
-# xset r rate (delay in ms) (repeat in hz)
 export DISPLAY=:0
-xset r rate 200 100
+
+# xset r rate (delay in ms) (repeat in hz)
+xset r rate 300 150
+
+# make caps ctrl
+setxkbmap -option altwin:swap_alt_win
+# use osx alt + super layout
+setxkbmap -option caps:ctrl_modifier
