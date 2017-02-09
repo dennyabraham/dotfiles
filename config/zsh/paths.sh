9117c4ee9d8949f3bd51acb29b073a8353553040
dennyabraham <dennyabraham@users.noreply.github.com>
2017-02-08 21:15:29 -0600

    add android home path

--- config/zsh/paths.sh
+++ config/zsh/paths.sh
@@ -1,3 +1,4 @@
-export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH"
+# TODO: split line into multiple invocations
+export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
 export MANPATH="$BREW_PREFIX/share/man:$MANPATH"
 export INFOPATH="$BREW_PREFIX/share/info:$INFOPATH"
--- tag-osx/config/zsh/arch.sh
+++ tag-osx/config/zsh/arch.sh
@@ -2,6 +2,7 @@ export BREW_PREFIX="/usr/local"
 export HOST_PROMPT_COLOR="green"
 export LOGIN_COLOR=$(printf "%03d" $(((RANDOM % 230) + 1)))
 export TMUX_DEFAULT_COMMAND="reattach-to-user-namespace -l $SHELL"
+export ANDROID_HOME=${HOME}/Library/Android/sdk
 
 if [ -e "$BREW_PREFIX/bin/docker-machine" ]; then
   eval "$(docker-machine env dev)"
