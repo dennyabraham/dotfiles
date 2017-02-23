ed3d2388bc716c8c2c829a869348652deeca58f0
dennyabraham <dennyabraham@users.noreply.github.com>
2017-02-23 12:56:22 -0600

    explicitly decide to use docker machine
    the other thing got really slow

--- tag-osx/config/zsh/arch.sh
+++ tag-osx/config/zsh/arch.sh
@@ -4,6 +4,6 @@ export LOGIN_COLOR=$(printf "%03d" $(((RANDOM % 230) + 1)))
 export TMUX_DEFAULT_COMMAND="reattach-to-user-namespace -l $SHELL"
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 
-if [ -e "$BREW_PREFIX/bin/docker-machine" ]; then
+if [ -n "$USE_DOCKER_MACHINE" ]; then
   eval "$(docker-machine env dev)"
 fi
