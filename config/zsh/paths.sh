fa09d3b33c9871013e873fd3a87b6bf6be798c8d
dennyabraham <dennyabraham@users.noreply.github.com>
2017-03-06 19:46:36 -0600

    add cargo path + rearrange path

--- config/zsh/paths.sh
+++ config/zsh/paths.sh
@@ -1,4 +1,4 @@
 # TODO: split line into multiple invocations
-export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$PATH:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
+export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:$HOME/.cargo/bin:$PATH"
 export MANPATH="$BREW_PREFIX/share/man:$MANPATH"
 export INFOPATH="$BREW_PREFIX/share/info:$INFOPATH"
