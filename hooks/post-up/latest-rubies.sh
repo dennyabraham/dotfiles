ab0b7e544bb82406f89ef4f97381ded8239a21d6
dennyabraham <dennyabraham@users.noreply.github.com>
2017-04-28 09:50:49 -0500

    ehh maybe keep doing that check
    This reverts commit e550efeffd96073488a5c7068af9a05f5e62c355.

--- hooks/post-up/latest-rubies.sh
+++ hooks/post-up/latest-rubies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ "$DOTFILE_UPDATE" = "force" ]; then
-  echo "[updating rubies]"
-  ruby-install -L > /dev/null
-else
-  echo "[not updating rubies]"
-fi
+ruby-install -L > /dev/null
