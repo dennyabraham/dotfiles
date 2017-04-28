5a94af3c4277c106cec07a4702380951c18e7cdf
dennyabraham <dennyabraham@users.noreply.github.com>
2017-04-28 09:26:03 -0500

    dotfiles color hook

---
+++ hooks/post-up/generate-login-color.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CONFIG_FILE="$HOME/.config/zsh/machine.sh"
+MACHINE_COLOR="$(printf "%03d" $(((RANDOM % 230) + 1)))"
+
+echo $CONFIG_FILE
+echo $MACHINE_COLOR
+
+if [ -e $CONFIG_FILE ]; then
+  if [ "$DOTFILE_UPDATE" = "regenerate" ]; then
+    echo "[regenerating machine color]"
+    echo "export MACHINE_COLOR=$MACHINE_COLOR" > $CONFIG_FILE
+  else
+    echo "[machine color already generated!]"
+  fi
+else
+  echo "[generating machine color]"
+  echo "export MACHINE_COLOR=$MACHINE_COLOR" > $CONFIG_FILE
+fi
+
