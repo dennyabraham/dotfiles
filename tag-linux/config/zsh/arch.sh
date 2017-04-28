6066c9ac6f7bda498b78fa95ae9ae4a16585cccf
dennyabraham <dennyabraham@users.noreply.github.com>
2017-04-28 09:35:23 -0500

    use per-machine login color instead of per-shell

--- tag-linux/config/zsh/arch.sh
+++ tag-linux/config/zsh/arch.sh
@@ -1,6 +1,5 @@
 export BREW_PREFIX="$HOME/.linuxbrew"
 export HOST_PROMPT_COLOR="yellow"
-export LOGIN_COLOR=$(printf "%03d" $(((RANDOM % 230) + 1)))
 export TMUX_DEFAULT_COMMAND="$SHELL"
 
 # this might not work outside of a running x instance
--- tag-osx/config/zsh/arch.sh
+++ tag-osx/config/zsh/arch.sh
@@ -1,6 +1,5 @@
 export BREW_PREFIX="/usr/local"
 export HOST_PROMPT_COLOR="green"
-export LOGIN_COLOR=$(printf "%03d" $(((RANDOM % 230) + 1)))
 export TMUX_DEFAULT_COMMAND="reattach-to-user-namespace -l $SHELL"
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 
