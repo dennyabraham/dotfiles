75e2dfe41965eb992a83e4336a76d3d2a4c5787a
dennyabraham <dennyabraham@users.noreply.github.com>
2017-09-25 11:03:53 -0500

    rm homebrew cask opts

--- config/zsh/vars.sh
+++ config/zsh/vars.sh
@@ -1,5 +1,3 @@
 export EDITOR=$BREW_PREFIX/bin/nvim
 export VISUAL=$EDITOR
 export ZSH_CUSTOM=$HOME/.config/zsh
-# FIXME: remove this
-export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
