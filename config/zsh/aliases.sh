dd5b6410c7cbae362aea684601a253e383231a94
dennyabraham <dennyabraham@users.noreply.github.com>
2018-09-21 12:22:50 -0500

    no dawker

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,7 +1,5 @@
 alias be="bundle exec"
 alias scpresume='rsync -Pazhv -e ssh'
-alias docker_machine_env="docker-machine env dev"
-alias new_docker_machine_env="docker-machine stop dev && docker-machine start dev && docker-machine env dev"
 alias journal='vim + "~/projects/journal/$(date +%Y)/$(date +%F).md"'
 alias nvimdiff="nvim -d"
 alias nvimlarge="nvim -u NONE"
