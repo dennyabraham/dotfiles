601a9cea350fbcc0e94d95208896ea7ae4d98234
dennyabraham <dennyabraham@users.noreply.github.com>
2019-02-04 14:38:55 -0600

    smart quotes

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -3,3 +3,4 @@ alias scpresume='rsync -Pazhv -e ssh'
 alias journal='vim + "~/projects/journal/$(date +%Y)/$(date +%F).md"'
 alias nvimdiff="nvim -d"
 alias nvimlarge="nvim -u NONE"
+alias rg="rg --smart-case"
