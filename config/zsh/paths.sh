01175024fe89ff296d85b3883842b70abf56dffa
dennyabraham <dennyabraham@users.noreply.github.com>
2019-08-22 14:44:18 -0500

    gopath

--- config/zsh/paths.sh
+++ config/zsh/paths.sh
@@ -1,4 +1,4 @@
 # TODO: split line into multiple invocations
-export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$HOME/.cargo/bin:$PATH"
+export PATH="$BREW_PREFIX/bin:$BREW_PREFIX/sbin:$HOME/.cargo/bin:$HOME/go/bin:$PATH"
 export MANPATH="$BREW_PREFIX/share/man:$MANPATH"
 export INFOPATH="$BREW_PREFIX/share/info:$INFOPATH"
