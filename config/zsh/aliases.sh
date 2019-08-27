877b01c0f86381d10655d627e82f1edcffe564f8
dennyabraham <dennyabraham@users.noreply.github.com>
2019-08-27 09:40:02 -0500

    diff sans whitespace

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -1,7 +1,7 @@
 alias be="bundle exec"
 alias scpresume='rsync -Pazhv -e ssh'
 alias journal='vim + "~/projects/journal/$(date +%Y)/$(date +%F).md"'
-alias nvimdiff="nvim -d"
+alias nvimdiff="nvim -d -c 'set diffopt+=iwhite'"
 alias nvimlarge="nvim -u NONE"
 alias rg="rg --smart-case"
 alias weather="curl -s http://wttr.in/60612?lang=en"
