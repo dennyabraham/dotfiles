f771011b90ebfac78389f304c921b43f036ab974
dennyabraham <dennyabraham@users.noreply.github.com>
2019-12-27 23:19:03 -0600

    update tmux copy config

--- tag-osx/config/zsh/arch.sh
+++ tag-osx/config/zsh/arch.sh
@@ -1,5 +1,4 @@
 export BREW_PREFIX="/usr/local"
-export TMUX_DEFAULT_COMMAND="reattach-to-user-namespace -l $SHELL"
 export ANDROID_HOME=${HOME}/Library/Android/sdk
 
 if [ -n "$USE_DOCKER_MACHINE" ]; then
