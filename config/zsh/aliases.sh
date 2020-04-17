4a6482aae266f6f2881e96530fce783debc5a829
dennyabraham <dennyabraham@users.noreply.github.com>
2020-04-16 21:02:50 -0500

    neovim pager

--- config/zsh/aliases.sh
+++ config/zsh/aliases.sh
@@ -3,5 +3,6 @@ alias scpresume='rsync -Pazhv -e ssh'
 alias journal='vim + "~/projects/journal/$(date +%Y)/$(date +%F).md"'
 alias nvimdiff="nvim -d -c 'set diffopt+=iwhite'"
 alias nvimlarge="nvim -u NONE"
+alias nvimpage="nvim -R"
 alias rg="rg --smart-case"
 alias weather="curl -s http://wttr.in/60612?lang=en"
