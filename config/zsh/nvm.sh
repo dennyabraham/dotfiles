3bc66510bcb372082f468c55f18e609924e289df
dennyabraham <dennyabraham@users.noreply.github.com>
2021-07-12 07:24:47 -0500

    nvm

--- config/zsh/nvm.sh
+++ config/zsh/nvm.sh
@@ -1,2 +1,3 @@
 export NVM_DIR="$HOME/.nvm"
-source "$BREW_PREFIX/nvm.sh"
+[ -s "$BREW_PREFIX/opt/nvm/nvm.sh" ] && . "$BREW_PREFIX/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "$BREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$BREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
