33a5f704e87b9f49928b47e2ee12151153c8849f
dennyabraham <dennyabraham@users.noreply.github.com>
2022-04-30 21:12:47 -0500

    conditional bash

--- config/zsh/brew.sh
+++ config/zsh/brew.sh
@@ -1,2 +1,12 @@
-eval $(/opt/homebrew/bin/brew shellenv)
+
+if [[ -e /usr/local/bin/brew ]]
+then
+  export BREW_LOCATION="/usr/local/bin/brew"
+elif [[ -e /home/linuxbrew/.linuxbrew/bin/brew ]]
+then
+  export BREW_LOCATION="/home/linuxbrew/.linuxbrew/bin/brew"
+else
+  export BREW_LOCATION="/opt/homebrew/bin/brew"
+fi
+eval $($BREW_LOCATION shellenv)
 export BREW_PREFIX=$(brew --prefix)
