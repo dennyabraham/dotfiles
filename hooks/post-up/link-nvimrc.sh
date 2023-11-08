659b6a94f4421079a6798a5591027c7272d8349b
dennyabraham <dennyabraham@users.noreply.github.com>
2023-11-07 22:18:24 -0500

    feat: no linking vimrc

--- hooks/post-up/link-nvimrc.sh
+++ hooks/post-up/link-nvimrc.sh
@@ -6,11 +6,7 @@ if [ -d "$HOME/.vim/" ]; then
     mkdir -p $HOME/.config/nvim
     ln -nfs $BREW_PREFIX/bin/nvim $BREW_PREFIX/bin/vim
     ln -nfs $BREW_PREFIX/bin/nvim $BREW_PREFIX/bin/vi
-    ln -nfs $HOME/.vimrc $HOME/.config/nvim/init.vim
   else
     echo "[nvim config already linked]"
   fi
-else
-  echo "[linking nvim config]"
-  ln -nfs $HOME/.vimrc $HOME/.config/nvim/init.vim
 fi
