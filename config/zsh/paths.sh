2f1ab730df05cb099447372b21d6896ba8aaf469
dennyabraham <dennyabraham@users.noreply.github.com>
2023-11-07 19:11:40 -0500

    feat: additional local paths

--- config/zsh/paths.sh
+++ config/zsh/paths.sh
@@ -1,6 +1,6 @@
 # use python3 as python
 export PATH=$PATH:$(brew --prefix python)/libexec/bin
-export PATH=$PATH:$HOME/.bin
+export PATH=$PATH:$HOME/.bin:$HOME/.local/bin
 export PATH=$PATH:$HOME/.cargo/bin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
